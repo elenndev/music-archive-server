cf93cae891c3c60453e94c193f99916b4b235315
Elen Damares <elen.damares774@gmail.com>
2024-11-18 09:16:24 -0300

    correcao da string

--- middleware.py
+++ middleware.py
@@ -10,6 +10,5 @@ AUTH_API_URL = os.environ.get('AUTH_API_URL')
 def validate_auth(token):
     headers = {
         "Authorization": f"Bearer {token}"}
-    response = requests.get(f"{AUTH_API_URL
-    }/check-token", headers = headers)
+    response = requests.get(f"{AUTH_API_URL}/check-token", headers = headers)
     return response
