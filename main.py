9b8c8860f7d7f84aaf560e7059f8fd9d16613d40
Elen Damares <elen.damares774@gmail.com>
2024-12-05 11:21:36 -0300

    metodo strip na variavel do sitemalxml

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def start():
 
 @app.get("/sitemap", response_class = Response)
 def sitemap() -> Response:
-    sitemap_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+    sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
         <loc>https://music-archive-blog.vercel.app</loc>
@@ -51,9 +51,9 @@ def sitemap() -> Response:
         <changefreq>yearly</changefreq>
         <priority>0.6</priority>
       </url>
-    </urlset>'''
+    </urlset>"""
     return Response(
-        content=sitemap_xml,
+        content=sitemap_xml.strip(),
         media_type="application/xml",
         headers={"Cache-Control": "max-age=3600"}
     )
